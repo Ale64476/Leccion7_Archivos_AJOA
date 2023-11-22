ae5cdffd2c7f54e9a113604f61e31ca87f3eee9e
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 00:24:51 -0600

    1 ejemplo

---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Extension/Animal.java
@@ -0,0 +1,23 @@
+package Ejemplos.Extension;
+
+public class Animal {
+    private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void hacerSonido() {
+        System.out.println("Un sonido de animal");
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public int obtenerEdad() {
+        return edad;
+    }
+}
---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Extension/Gato.java
@@ -0,0 +1,15 @@
+package Ejemplos.Extension;
+
+public class Gato extends Animal{
+    
+    private boolean esDomestico;
+
+    public Gato(String nombre, int edad, boolean esDomestico) {
+        super(nombre, edad);
+        this.esDomestico = esDomestico;
+    }
+    public void maullar() {
+        System.out.println("¡Meow!");
+    }
+  
+}
