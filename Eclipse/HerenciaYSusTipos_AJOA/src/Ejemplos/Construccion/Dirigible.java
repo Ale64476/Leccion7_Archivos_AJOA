281a4b5e09c797bf5d9b9b5d1ca5dc9e31e1cd68
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 01:50:30 -0600

    todos mis ejemplos

---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Construccion/Dirigible.java
@@ -0,0 +1,23 @@
+package Ejemplos.Construccion;
+
+public class Dirigible extends Avion{
+
+	private boolean publicidad;
+	private String dimensiones;
+	
+	
+	public void Navegar() {
+		
+	}
+	public void Ascender() {
+		
+	}
+
+	public boolean getPublicidad() {
+		return publicidad;
+	}
+	
+	public String getDimensiones() {
+		return dimensiones;
+	}
+}
