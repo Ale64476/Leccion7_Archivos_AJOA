44dc0f77171f9aa75ff35384c4aeabf2ccda26a2
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 01:34:23 -0600

    Resto de ejemplos

---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Extension/Entrenador.java
@@ -0,0 +1,13 @@
+package Ejemplos.Extension;
+
+public class Entrenador extends SeleccionFutbol{
+
+	private String idFederacion;
+
+	public void dirigirPartido() {
+		System.out.println("Dirige el partido");
+	}
+
+	public void dirigirEntreno() {
+	}
+}
