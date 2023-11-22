7066838aab36e07eb893130481d125bb3168f71b
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 00:33:02 -0600

    ejemplos

---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Extension/AnimalControladora.java
@@ -0,0 +1,19 @@
+package Ejemplos.Extension;
+
+public class AnimalControladora {
+    public static void main(String[] args) {
+        
+        Perro miPerro = new Perro("Puppy", 1, "Maltez");
+        miPerro.hacerSonido(); 
+        miPerro.ladrar(); 
+
+        System.out.println("Nombre del perro: " + miPerro.obtenerNombre());
+
+        Gato miGato = new Gato("Mandarino", 3, true);
+        miGato.hacerSonido(); 
+        miGato.maullar(); 
+
+        System.out.println("Edad del gato: " + miGato.obtenerEdad());
+    }
+ }
+    
---
+++ Eclipse/HerenciaYSusTipos_AJOA/src/Ejemplos/Extension/Perro.java
@@ -0,0 +1,15 @@
+package Ejemplos.Extension;
+
+public class Perro extends Animal {
+    
+    private String raza;
+
+   public Perro(String nombre, int edad, String raza) {
+       super(nombre, edad);
+       this.raza = raza;
+   }
+
+   public void ladrar() {
+       System.out.println("¡Guau!");
+   }
+}
